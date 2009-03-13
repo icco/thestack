3b2652e6d60897a53d91cf1a285d5ec28fd3a3a1
Nat Welch <nat@natwelch.com>
2009-03-12 23:46:22 -0700

    General layout.
    Not sure if I actually want to code tonight. Just messing around for
    now, figuring out general layout and such. It is finals week. I should
    do my calc hw...

---
+++ README.md
@@ -0,0 +1,5 @@
+# theStack!
+
+Based upon [the data structure][1].
+
+ [1]: http://en.wikipedia.org/wiki/Stack_(data_structure)
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Connect to database
+
+// build two arrays, one for today, one for yesterday
+// Function: give it a date, gives you an array
+
+// print it out like it's 1992
+
+
+?>
+
