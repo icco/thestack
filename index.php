b7a5db624efa07b446b280cc9ebc0d490c118246
Nat Welch <nat@natwelch.com>
2009-05-07 19:26:02 -0700

    Basically have converted mootools demo.

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
-
-// Connect to database
-
-// build two arrays, one for today, one for yesterday
-// Function: give it a date, gives you an array
-
-// print it out like it's 1992
+/*
+ *
+ * Connect to database
+ *
+ * Function: give it a date, gives you an array
+ *
+ * print it out like it's 1992
+ *
+ */
 
 include "backend.php";
 
 ?>
-
-<html>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="demo.css" type="text/css" />
 	<script type="text/javascript" src="mootools-core.js"></script>
 	<script type="text/javascript" src="mootools-more.js"></script>
-	<script type="text/javascript" src="demo.js"></script>
+	<script type="text/javascript" src="stack.js"></script>
+	<link rel="stylesheet" href="stack.css" type="text/css" />
 <title>the Stack!</title>
 </head>
 <body>
-<h1>theStack!</h1>
-<p>OMG. Content goes here.</p>
+<div id="cont">
+	<h1>theStack!</h1>
+	<p>OMG. Content goes here.</p>
 
 	<form id="addTask">
 		<input type="text" id="newTask" />
@@ -36,5 +37,6 @@ include "backend.php";
 	</div>
 
 	<div id="data"/>
+	</div>
 </body>
 </html>
