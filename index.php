e9b1ce226d4d851bb7a570b2de986b31709e3aef
Nat Welch <nat@natwelch.com>
2009-05-07 19:17:04 -0700

    meh.

--- index.php
+++ index.php
@@ -12,26 +12,29 @@ include "backend.php";
 ?>
 
 <html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="demo.css" type="text/css" />
+	<script type="text/javascript" src="mootools-core.js"></script>
+	<script type="text/javascript" src="mootools-more.js"></script>
+	<script type="text/javascript" src="demo.js"></script>
 <title>the Stack!</title>
 </head>
 <body>
 <h1>theStack!</h1>
 <p>OMG. Content goes here.</p>
-<form action="index.php" method="post">
-<textarea name="text"></textarea>
-<br /><input type="submit" value="Submit" />
-</form>
-<p>
-<?php
-print $_POST['text'];
-?>
-</p><p>
-<?php
-$temp = new Post();
-print $temp;
-?>
-</p>
-</body>
-</head>
 
+	<form id="addTask">
+		<input type="text" id="newTask" />
+		<input type="submit" value="Add Task" />
+	</form>
+
+	<div id="listArea">
+		<ol id="todo"></ol>
+	</div>
+
+	<div id="data"/>
+</body>
+</html>
