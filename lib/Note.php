af7095d74f23abbe6e4c19f5642594ad97abf4b6
Nat Welch <nat@natwelch.com>
2009-05-28 19:19:13 -0700

    stubbing a few things out.

--- lib/Note.php
+++ lib/Note.php
@@ -3,6 +3,93 @@
 public class Note 
 {
 
+	/**
+	 * The constructor. A brand new note.
+	 */
+	function __construct()
+	{
+		$this->setX(100);
+		$this->setY(100);
+		$this->setColor("#CC6699");
+		$this->setText("This is a new note.");
+	}
+
+	function toHTML()
+	{
+
+	}
+
+	/**
+	 * Sets the x position of the note.
+	 *
+	 * @param $in An integer of distance from left edge of window.
+	 */
+	function setX($in)
+	{
+
+	}
+
+	/**
+	 * Sets the y position of the note.
+	 *
+	 * @param $in An integer of distance from top edge of window.
+	 */
+	function setY($in)
+	{
+
+	}
+
+	/**
+	 * Sets the content of the note.
+	 *
+	 * @param $in The text you wish to store in the note.
+	 */
+	function setText($in)
+	{
+
+	}
+
+	/**
+	 * Sets the color of the note
+	 *
+	 * @param $in the hex value of the color you wish to set the note to.
+	 */
+	function setColor($in)
+	{
+
+	}
+
+	/**
+	 * Gets the x position of the note.
+	 */
+	function getX()
+	{
+		return $this->x;
+	}
+
+	/**
+	 * Gets the y position of the note.
+	 */
+	function getY()
+	{
+		return $this->y;
+	}
+
+	/**
+	 * Gets the content of the note.
+	 */
+	function getText()
+	{
+		return $this->text;
+	}
+
+	/**
+	 * Sets the color of the note
+	 */
+	function getColor()
+	{
+		return $this->color;
+	}
 
 
 }
