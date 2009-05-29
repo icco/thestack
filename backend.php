1c2a748fe7b0a07d59d6ec0ef94017ec3f972d6a
Nat Welch <nat@natwelch.com>
2009-05-28 18:35:49 -0700

    This doesn't work

--- backend.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$text = $_GET['newTask'];
-
-if(isset($text))
-	file_put_contents("ToDo.txt","<li> $text </li> \n", FILE_APPEND | LOCK_EX);
-
-?>
--- getList.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo file_get_contents("ToDo.txt");
-?>
