2fd0f309603a8f9cda24394854f3a987da9a65b0
Nat Welch <nat@natwelch.com>
2010-03-15 10:48:46 -0700

    Deleting comment.

--- main.rb
+++ main.rb
@@ -8,9 +8,6 @@ require 'less'
 configure :production do
    # Configure stuff here you'll want to
    # only be run at Heroku at boot
-
-   # TIP:  You can get you database information
-   #       from ENV['DATABASE_URI'] (see /env route below)
 end
 
 # Quick test
