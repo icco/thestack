d41750fbb6ea9aeb926dd1923be98cfb8086155a
Nat Welch <nat@natwelch.com>
2010-03-15 09:55:31 -0700

    Alrighty. Let's learn ruby.

--- main.rb
+++ main.rb
@@ -1,5 +1,29 @@
+# You'll need to require these if you
+# want to develop while running with ruby.
+# The config/rackup.ru requires these as well
+# for it's own reasons.
+#
+# $ ruby main.rb
+#
 require 'rubygems'
 require 'sinatra'
-get '/hi' do
-     "Hello World!"
+
+configure :production do
+   # Configure stuff here you'll want to
+   # only be run at Heroku at boot
+
+   # TIP:  You can get you database information
+   #       from ENV['DATABASE_URI'] (see /env route below)
 end
+
+# Quick test
+get '/' do
+     "Congradulations!
+        You're running a Sinatra application on Heroku!"
+end
+
+# TODO: Delete...
+get '/env' do
+  ENV.inspect
+end
+
