740514bd341669867ea410f238e1fd58a7b100ee
Nat Welch <nat@natwelch.com>
2010-03-16 00:02:23 -0700

    Insertion works...

--- main.rb
+++ main.rb
@@ -10,13 +10,15 @@ require 'sequel'
 configure do
    set :sessions, true
    DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://thestack.db')
+   DB.drop_table :posts
    DB.create_table :posts do
       primary_key :postid
-      String :text
-      Int :date
+      Text :text
+      integer :date
    end
 
 end
+
 
 configure :production do
    # Configure stuff here you'll want to
@@ -28,6 +30,9 @@ get '/' do
 end
 
 post '/' do
+   DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://thestack.db')
+   data = DB[:posts]
+   data.insert(:text => params['text'], :date => Time.now.to_i)
    erubis :view, :locals => {:hello => params['text']}
 end
 
